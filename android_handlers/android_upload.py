adabc4bbb0a5fe89df21fc60ba92169f0b9748d2
hidemon <yusun@utexas.edu>
2015-11-07 00:15:26 -0600

    Foundations

--- android_handlers/android_upload.py
+++ android_handlers/android_upload.py
@@ -1,8 +1,7 @@
 import webapp2
 from models import JINJA_ENVIRONMENT
 
-class LoginHandler(webapp2.RequestHandler):
+class AndroidUploadHandler(webapp2.RequestHandler):
     def get(self):
         template = JINJA_ENVIRONMENT.get_template('htmls/login.html')
         self.response.write(template.render())
-
